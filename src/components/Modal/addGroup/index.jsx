31d431616e1aa8d1169e16ac453c9a2664d8f1d5
luizpcardoso <luizpauloreiscardosoo@gmail.com>
2021-12-09 17:40:25 -0300

    fix(addGroup): Change provider function

--- src/components/Modal/addGroup/index.jsx
+++ src/components/Modal/addGroup/index.jsx
@@ -5,12 +5,14 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup"
 import { api } from "../../../services/api";
+import { useGroups } from "../../../providers/groups"
 
 
 const AddGroupModal = () => {
 
 
-    const { userData } = useUser()
+
+    const { createGroup } = useGroups()
 
     const formSchema = yup.object().shape({
         name: yup.string().required("Informe o título"),
@@ -33,19 +35,9 @@ const AddGroupModal = () => {
             "category": infos.category,
 
         }
-
-        api
-            .post(`/groups/`, data, {
-                headers: {
-                    Authorization: `Bearer ${userData.token}`,
-                },
-            })
-            .then((response) => {
-                console.log(response)
-
-            })
-            .catch((error) => console.log(error));
+        createGroup(data)
     }
+
     return (
         <Container>
             <form onSubmit={handleSubmit(onSubmitFunction)}>
