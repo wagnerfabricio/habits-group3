6b7a512c0ec27d485b1d5d5461950d6b38b26afc
luizpcardoso <luizpauloreiscardosoo@gmail.com>
2021-12-11 14:20:53 -0300

    Feat(addHabit): Make button to open Modal addHabit and create inputs radio in addHabit

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,31 @@
+import { ButtonAdd } from "../../components/ButtonAdd"
+import { Modal, Box } from "@mui/material";
+import AddHabitModal from "../../components/Modal/addHabit";
+import { useState } from "react";
+
+
 const Dashboard = () => {
+
+    const [openAddHabit, setOpenAddHabit] = useState(false);
+    const handleOpenAddHabit = () => setOpenAddHabit(true);
+    const handleCloseAddHabit = () => setOpenAddHabit(false);
 
     return (
 
+        <>
 
-        <div>Dashboard</div>
+            <div>Dashboard</div>
 
+            <Modal
+                open={openAddHabit}
+                onClose={handleOpenAddHabit}
+            >
+                <Box>
+                    <AddHabitModal handleCloseAddHabit={handleCloseAddHabit} />
+                </Box>
+            </Modal>
+
+        </>
     )
 }
 
