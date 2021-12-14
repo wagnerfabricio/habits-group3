7e883b1adafcd35f76fe63b73398b4db8fb30927
luizpcardoso <luizpauloreiscardosoo@gmail.com>
2021-12-14 15:37:13 -0300

    feature(EditHabit): Create files (not complete)

---
+++ src/components/Modal/editHabit/index.jsx
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react";
+import { Container } from "./style";
+import { useUser } from "../../../providers/user";
+import { useHabits } from "../../../providers/habits.js"
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"
+import Button from '../../Button/index'
+import { toast } from "react-toastify";
+
+
+
+const EditHabitModal = ({ handleCloseAddHabit }) => {
+
+    const { userData } = useUser()
+    const { createHabit } = useHabits()
+
+    const formSchema = yup.object().shape({
+        title: yup.string().required("Informe o título"),
+        category: yup.string().required("Informe a categoria"),
+        difficulty: yup.string().required("qual a dificuldade?"),
+        frequency: yup.string().required("qual a frequência?"),
+
+    })
+
+    const { register, handleSubmit, formState: { errors } } = useForm(
+        {
+            resolver: yupResolver(formSchema),
+        }
+    )
+
+
+
+    const onSubmitFunction = (infos) => {
+
+
+        const data = {
+            "title": infos.title,
+            "category": infos.category,
+            "difficulty": infos.difficulty,
+            "frequency": infos.frequency,
+            "achieved": false,
+            "how_much_achieved": 0,
+            "user": userData.user.id
+        }
+        createHabit(data);
+        handleCloseAddHabit()
+    }
+    useEffect(() => {
+        if (Object.keys(errors).length > 0) {
+            toast.error("Preencha todos os campos corretamente...")
+
+        }
+
+    }, [errors])
+
+
+
+
+
+
+    return (
+        <Container>
+            <div className="headder">
+                <h2>Adicionar hábito</h2>
+                <button onClick={() => { handleCloseAddHabit() }}>X</button>
+            </div>
+            <form onSubmit={handleSubmit(onSubmitFunction)}>
+
+                <input className="inputText" defaultValue="tit" {...register("title")}></input>
+
+                <input className="inputText" defaultValue="cat" {...register("category")}></input>
+
+
+                <fieldset className="fildDificult"  >
+                    <legend>Dificuldade</legend>
+                    <div className="divRadio">
+                        <input className="radio" type="radio" id="dificultEasy" name="dificult" value="Fácil" {...register("difficulty")} />
+                        <label htmlFor="dificultEasy">Fácil</label>
+                    </div>
+                    <div className="divRadio">
+                        <input className="radio" type="radio" id="dificultMedium" name="dificult" value="Moderada" {...register("difficulty")} />
+                        <label htmlFor="dificultMedium">Moderada</label>
+                    </div>
+                    <div className="divRadio">
+                        <input className="radio" type="radio" id="dificultHard" name="dificult" value="Difícil" {...register("difficulty")} />
+                        <label htmlFor="dificultHard">Difícil</label>
+                    </div>
+                </fieldset>
+                <fieldset className="fildfrequency" >
+                    <legend>Frequência</legend>
+                    <div className="divRadio">
+                        <input className="radio" type="radio" id="frequencyWeekly" name="frequency" value="Diária" {...register("frequency")} />
+                        <label htmlFor="frequencyDaily">Diária</label>
+                    </div>
+                    <div className="divRadio">
+                        <input className="radio" type="radio" id="frequencyWeekly" name="frequency" value="Semanal"  {...register("frequency")} />
+                        <label htmlFor="frequencyWeekly">Semanal</label>
+                    </div>
+                    <div className="divRadio">
+                        <input className="radio" type="radio" id="frequencyMonthly" name="frequency" value="Mensal" {...register("frequency")} />
+                        <label htmlFor="frequencyMonthly">Mensal</label>
+                    </div>
+                </fieldset>
+
+                <Button type="submit">Editar hábito</Button>
+            </form>
+        </Container>
+    )
+
+}
+
+export default EditHabitModal
---
+++ src/components/Modal/editHabit/style.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 500px;
+  width: 400px;
+  background-color: #541da8;
+  display: flex;
+  flex-direction: column;
+  margin: 20vh auto;
+  align-items: center;
+  border-radius: 15px;
+  box-shadow: 3px 5px 5px #00000080;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+
+  form {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .headder {
+    display: flex;
+    margin: 0 0 30px 0;
+  }
+  .headder button {
+    display: flex;
+    align-items: center;
+    height: 20px;
+    width: 20px;
+    justify-content: center;
+    border-radius: 100%;
+    margin: 0;
+    border: none;
+    color: #ffffff;
+    background-color: #ffffff00;
+    font-size: 18px;
+    font-weight: bold;
+    position: absolute;
+    margin: 10px 0 0 280px;
+  }
+  h2 {
+    margin: 40px auto 0 auto;
+    color: #ffffff;
+    font-size: 28px;
+  }
+  .inputText {
+    background-color: #ffffff;
+    border-radius: 5px;
+    height: 20px;
+    margin: 20px 0 0 0;
+    height: 45px;
+    width: 320px;
+    border: none;
+    padding-left: 10px;
+  }
+  .radio {
+    height: 15px;
+    width: 40px;
+    margin: 0;
+  }
+  .divRadio {
+    color: #ffffff;
+    display: flex;
+    align-items: flex-end;
+    width: 110px;
+  }
+  fieldset legend {
+    color: #ffffff;
+    font-size: 20px;
+    margin-bottom: 5px;
+  }
+  .fildDificult,
+  .fildfrequency {
+    display: flex;
+    width: 300px;
+    border: none;
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0 5px 0;
+  }
+  button {
+    margin-top: 50px;
+  }
+`;
