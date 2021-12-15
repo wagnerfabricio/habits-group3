c71cf314552730cb923883e6d91a0475af1c985c
felipefoster <88387736+felipefoster@users.noreply.github.com>
2021-12-15 18:43:12 -0300

    style/dashboard
    styled dashboard, footer and aside

--- src/components/Aside/styles.js
+++ src/components/Aside/styles.js
@@ -1,14 +1,12 @@
 import styled from "styled-components";
+import { deepOrange } from "@mui/material/colors";
 
 export const Container = styled.aside`
   display: flex;
   flex-direction: column;
-  align-items: center; // remover depois
-  width: 100%;
-  max-width: 800px;
+  /* width: 100%; */
+  /* max-width: 600px; */
   height: 100%;
-
-  padding: 0 20px;
 `;
 
 export const UpperSection = styled.section`
@@ -18,9 +16,10 @@ export const UpperSection = styled.section`
   align-items: center;
   text-align: center;
   width: 100%;
+  min-width: 320px;
   height: 60%;
-  padding: 20px 0;
-  filter: drop-shadow(10px 7px 4px rgba(0, 0, 0, 0.25));
+  padding: 20px;
+
   border-radius: 15px;
   background: #39006e;
   color: #ffffff;
@@ -41,28 +40,67 @@ export const Avatar = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 130px;
-  height: 139px;
+  width: 100px;
+  height: 100px;
   background: #ffffff;
   border-radius: 50%;
 `;
 
+export const UserContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  width: 70%;
+  height: 100%;
+  max-height: 200px;
+  padding: 20px;
+`;
+
+export const UserContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 50%;
+  height: 100%;
+  max-height: 150px;
+  padding: 20px;
+  gap: 10px;
+`;
+
 export const CardsContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
   width: 100%;
+
   gap: 10px;
   height: 100%;
   padding: 10px;
 `;
 
-export const Cards = styled.div`
-  width: 35%;
+export const CardButton = styled.div`
+  display: flex;
+
+  color: black;
+  width: 70%;
   height: 130px;
   background: white;
   padding: 10px;
+  cursor: pointer;
+  border-radius: 15px 0 15px 0;
+  border-right: 4px solid transparent;
+
+  :hover {
+    border-right: 4px solid ${deepOrange[500]};
+  }
+  img {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 export const LowerSection = styled.section`
@@ -70,110 +108,45 @@ export const LowerSection = styled.section`
   flex-direction: column;
   align-items: center;
   height: 40%;
+  padding: 10px;
 `;
 
 export const StatsContainer = styled.section`
-  width: 100%;
+  /* width: 20%; */
+  max-width: 800px;
   margin: 10px;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   padding: 10px;
-  gap: 0 50px;
+  gap: 10px;
+  font-size: 2vh;
+  font-style: bolder;
 `;
 
-export const StatsActivities = styled.div`
+export const Stats = styled.div`
   display: flex;
-
+  flex: 1;
+  padding: 5px;
   text-align: center;
   flex-direction: column;
   align-items: center;
-  span {
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 60px;
-
-    color: blue;
-  }
-
-  p {
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 30px;
-    line-height: 35px;
-    color: blue;
-  }
+  font-size: 2rem;
 `;
 
-export const StatsGroups = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  align-items: center;
-  span {
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 60px;
+export const Button = styled.button`
+  background: none;
+  color: #ffffff;
+  height: 30px;
+  border-radius: 0.5rem;
+  border: none;
+  width: 30px;
+  max-width: 336px;
+  font-weight: 600;
+  transition: 0.5s;
 
-    color: red;
-  }
-
-  p {
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 30px;
-    line-height: 35px;
-    color: red;
-  }
-`;
-
-export const StatsHabits = styled.div`
-  display: flex;
-  align-items: center;
-  text-align: center;
-  flex-direction: column;
-  span {
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 60px;
-
-    color: #000000;
-  }
-
-  p {
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 30px;
-    line-height: 35px;
-  }
-`;
-
-export const StatsGoals = styled.div`
-  display: flex;
-  align-items: center;
-  text-align: center;
-  flex-direction: column;
-  span {
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 60px;
-
-    color: green;
-  }
-
-  p {
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 30px;
-    line-height: 35px;
-    color: green;
+  margin-top: 1rem;
+  :hover {
+    border: 2px solid #f5f5f5;
   }
 `;
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
+  /* display: flex; */
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 50px;
+  padding: 10px;
+  .main {
+    display: none;
+    @media (min-width: 800px) {
+      display: block;
+    }
+  }
 `;
